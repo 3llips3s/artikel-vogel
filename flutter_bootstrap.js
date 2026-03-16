9c4c42ed98a18a157ec3e570e58228cfcd3c0ee6
3llips3s <164566346+3llips3s@users.noreply.github.com>
2026-03-16 19:11:09 +0000

    Deploying to gh-pages from @ 3llips3s/artikel-vogel@2275bccf5350b1dc962e622fecfd1924bc1cd901 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "969576951"
+    serviceWorkerVersion: "4089752815"
   }
 });
